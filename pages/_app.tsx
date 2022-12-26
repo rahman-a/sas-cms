ddef6c5cc35d983413c5ae5ace64c5fc82d927e4
rahman-a <ahm.abdrahman@gmail.com>
2022-12-26 22:29:07 +0200

    change the favicon from react bricks logo to sas-partner logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import config from '../react-bricks/config'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Head from 'next/head'
 
 // import '../css/style.css'
 import '@styles/globals.scss'
@@ -12,6 +13,9 @@ const queryClient = new QueryClient()
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <link rel='icon' href='/images/logo.webp' />
+      </Head>
       <ReactBricks {...config}>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
